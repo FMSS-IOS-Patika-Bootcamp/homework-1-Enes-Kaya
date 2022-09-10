cd3acdf8b07e444355b4576bbcc6ea4f9746d867
Enes Kaya <eng.eneskaya@gmail.com>
2022-09-10 14:45:26 +0300

    Add TopBar Item and App Logo

--- HW1-EnesKAYA/ViewController.swift
+++ HW1-EnesKAYA/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 //This class is about first page when app starts
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     //TableView
-    @IBOutlet weak var tableView: UITableView!
+    
     var petArray = [String]()
     var pet = ""
     
@@ -19,8 +19,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         
         
-        tableView.delegate = self
-        tableView.dataSource = self
+       
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
@@ -37,16 +36,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         pet = nameFromForm
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
     
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = pet
-        return cell
-    }
     
     
     
